aaeea093e622e540dbf67b4d50931c0de2abd1bb
menteMaesta <paulagomenr@gmail.com>
2024-06-21 20:14:33 -0600

    Install storybook and remove all components

--- src/index.js
+++ src/index.js
@@ -4,10 +4,3 @@ import * as icons from './shared/icons';
 import * as global from './shared/global';
 
 export { styles, animation, icons, global };
-
-export * from './Avatar';
-export * from './Badge';
-export * from './Button';
-export * from './Icon';
-export * from './Link';
-export * from './LinkWrapper';
