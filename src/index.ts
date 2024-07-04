bf0f6aa7166cf0bd04f44d3ff8cc591f397743b2
menteMaesta <paulagomenr@gmail.com>
2024-07-04 14:55:39 -0600

    Use aliases inside LoaderWrapper and index files

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import LoaderWrapper, {
   LoaderWrapperProps,
-} from './LoaderWrapper/LoaderWrapper';
-import FlowerSvg from './FlowerSvg/FlowerSvg';
-import AnglerFishSvg from './AnglerFishSvg/AnglerFishSvg';
-import './shared/main.css';
+} from 'src/LoaderWrapper/LoaderWrapper.tsx';
+import FlowerSvg from 'src/FlowerSvg/FlowerSvg.tsx';
+import AnglerFishSvg from 'src/AnglerFishSvg/AnglerFishSvg.tsx';
+import 'shared/main.css';
 
 export { LoaderWrapper, FlowerSvg, AnglerFishSvg };
 export type { LoaderWrapperProps };
