3b1210a6b965fd2817e2c993cd5d9da2c2aabb7d
menteMaesta <paulagomenr@gmail.com>
2024-07-11 15:28:51 -0600

    Remove unrecognized svg attributes and export new icons on index

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,17 @@ import LoaderWrapper, {
 } from 'src/LoaderWrapper/LoaderWrapper.tsx';
 import FlowerSvg from 'src/FlowerSvg/FlowerSvg.tsx';
 import AnglerFishSvg from 'src/AnglerFishSvg/AnglerFishSvg.tsx';
+import CatTailSvg from 'src/CatTailSvg/CatTailSvg.tsx';
+import BloDuckSvg from 'src/BloDuckSvg/BloDuckSvg.tsx';
+import SeaDogSvg from 'src/SeaDogSvg/SeaDogSvg';
 import 'shared/main.css';
 
-export { LoaderWrapper, FlowerSvg, AnglerFishSvg };
+export {
+  LoaderWrapper,
+  FlowerSvg,
+  AnglerFishSvg,
+  CatTailSvg,
+  BloDuckSvg,
+  SeaDogSvg,
+};
 export type { LoaderWrapperProps };
